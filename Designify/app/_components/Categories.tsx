67edc798d27b10d9dd570df8dcb68eaf234f8472
debasish-max <hazarikadebasish182@gmail.com>
2026-03-14 23:25:29 +0530

    its-done

--- Designify/app/_components/Categories.tsx
+++ Designify/app/_components/Categories.tsx
@@ -34,7 +34,7 @@ function Categories() {
       <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 mt-5 '>
         {categoryList?.map((category:Category, index:number) => (
             <Link href={'/category/' + category?.slug} key={category.documentId} className='p-4 border rounded-lg flex flex-col items-center hover:border-primary cursor-pointer'>
-                ={category?.icon?.url} width={80} height={80} alt={category.name}/>
+                <Image src={category?.icon?.url} width={80} height={80} alt={category.name}/>
                 <h2 className='text-lg font-medium'>{category?.name}</h2>
             </Link>
         ))}
