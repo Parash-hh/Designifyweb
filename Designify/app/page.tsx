554de67fd46a786716d4fce5ad3057d008c63a82
debasish-max <hazarikadebasish182@gmail.com>
2026-04-03 17:07:23 +0530

    3d-layout and animations

--- Designify/app/page.tsx
+++ Designify/app/page.tsx
@@ -4,6 +4,9 @@ import Hero from "./_components/Hero";
 import Categories from "./_components/Categories";
 import PopularProducts from "./_components/PopularProducts";
 import Footer from "./_components/Footer";
+import ThreeDSection from "./_components/ThreeDSection";
+import HowItWorksSection from "./_components/HowItWorksSection";
+import ScrollShowcase from "./_components/ScrollShowcase";
 
 export default function Home() {
   return (
@@ -11,13 +14,18 @@ export default function Home() {
 
     {/* Hero */}
     <Hero/>
+    
+    <ScrollShowcase/>
+
+    {/* How it works */}
+    <HowItWorksSection/>
 
     {/* Category list*/}
     <Categories/>
 
     {/* Product list*/}
     <PopularProducts/>
-
+<ThreeDSection/>
     {/* Footer */}
     <Footer/>
    </div>
